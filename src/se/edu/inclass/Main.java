5a7ba902026b40d02f1d1135d55a679118d25e95
theodorekwok <theodorekwok@gmail.com>
2021-09-17 17:38:41 +0800

    Use lambda functions to filter and sort

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -6,7 +6,11 @@ import se.edu.inclass.task.Task;
 import se.edu.inclass.task.TaskNameComparator;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 public class Main {
 
@@ -20,6 +24,9 @@ public class Main {
         printDeadlines(tasksData);
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+        printDeadlinesUsingStreams(tasksData);
+        ArrayList<Task> filteredList = filterTasksByString(tasksData, "11");
+        printData(filteredList);
 
     }
 
@@ -45,5 +52,18 @@ public class Main {
                 System.out.println(t);
             }
         }
+    }
+
+    public static void printDeadlinesUsingStreams(ArrayList<Task> tasksData) {
+        tasksData.stream()
+                .filter((t) -> t instanceof Deadline)
+                .sorted((a, b) -> a.getDescription().toLowerCase().compareTo(b.getDescription().toLowerCase()))
+                .forEach(System.out::println);
+    }
+
+    public static ArrayList<Task> filterTasksByString(ArrayList<Task> tasks, String filteredByString) {
+        return (ArrayList<Task>) tasks.stream()
+                .filter((t) -> t.getDescription().contains(filteredByString))
+                .collect(Collectors.toList());
     }
 }
